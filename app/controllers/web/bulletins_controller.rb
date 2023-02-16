9a3181fcde6c488fb2a4adc2157ce31e78e275f9
Hex <thehexreader@gmail.com>
2023-02-16 18:35:55 +0300

    saving progress

--- app/controllers/web/bulletins_controller.rb
+++ app/controllers/web/bulletins_controller.rb
@@ -5,9 +5,11 @@ module Web
     end
 
     def create
-      if Bulletin.create(params[:bulletin])
+      @bulletin = User.find_by(id: session[:user_id]).bulletins.build(bulletin_params)
+
+      if @bulletin.save
         flash[:notice] = t('success')
-        redirect_to bulletins_path
+        redirect_to root_path
       else
         flash[:notice] = t('fail')
         redirect_to root_path
@@ -16,6 +18,12 @@ module Web
 
     def show
       @bulletin = Bulletin.find_by(id: params[:id])
+    end
+
+    protected
+
+    def bulletin_params
+      params.require(:bulletin).permit(:title, :description, :category_id)
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :users
+  has_many :bulletins
 end
