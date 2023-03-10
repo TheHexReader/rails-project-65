a6cfff32ceb87b0cdf51f8a957356314b980ccf6
Hex <thehexreader@gmail.com>
2023-03-10 13:58:24 +0300

    fixing

--- db/migrate/20230302081243_add_admin.rb
+++ db/migrate/20230302081243_add_admin.rb
@@ -3,6 +3,8 @@
 # Add admin
 class AddAdmin < ActiveRecord::Migration[7.0]
   def change
-    User.find_by(email: 'thehexreader@gmail.com').update(admin: true)
+    user = User.find_by(email: 'thehexreader@gmail.com')
+    user ||= User.create!(email: 'thehexreader@gmail.com')
+    user.update(admin: true)
   end
 end
