9ba99c983c60381dce7d066ac3adc466c973c087
Hex <thehexreader@gmail.com>
2023-03-10 16:42:41 +0300

    added translations

--- app/controllers/web/admin/bulletins_controller.rb
+++ app/controllers/web/admin/bulletins_controller.rb
@@ -53,17 +53,17 @@ module Web
         @bulletin = Bulletin.find_by(id: params[:id]).archive!
         redirect_to admin_bulletins_path
       end
-  
+
       def moderate
         @bulletin = Bulletin.find_by(id: params[:id]).moderate!
         redirect_to admin_bulletins_path
       end
-  
+
       def publish
         @bulletin = Bulletin.find_by(id: params[:id]).publish!
         redirect_to admin_bulletins_path
       end
-  
+
       def reject
         @bulletin = Bulletin.find_by(id: params[:id]).reject!
         redirect_to admin_bulletins_path
