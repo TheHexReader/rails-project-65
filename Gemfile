abb5cd3cc9fee8a5ba3bc56e5539a258b660f66b
Hex <thehexreader@gmail.com>
2023-03-17 10:58:58 +0300

    fixing

--- Gemfile
+++ Gemfile
@@ -101,4 +101,4 @@ gem 'aasm', '~> 5.5'
 gem 'kaminari', '~> 1.2'
 gem 'ransack', '~> 4.0'
 
-gem "slim_lint", "~> 0.24.0"
+gem 'slim_lint', '~> 0.24.0'
--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -4,26 +4,26 @@ class Bulletin < ApplicationRecord
   include AASM
 
   aasm.attribute_name :state
-    aasm do
-      state :draft, initial: true
-      state :under_moderation, :published, :rejected, :archived
+  aasm do
+    state :draft, initial: true
+    state :under_moderation, :published, :rejected, :archived
 
-      event :moderate do
-        transitions from: :draft, to: :under_moderation
-      end
-
-      event :publish do
-        transitions from: :under_moderation, to: :published
-      end
-
-      event :reject do
-        transitions from: :under_moderation, to: :rejected
-      end
-
-      event :archive do
-        transitions from: %i[published under_moderation draft rejected], to: :archived
-      end
+    event :moderate do
+      transitions from: :draft, to: :under_moderation
     end
+
+    event :publish do
+      transitions from: :under_moderation, to: :published
+    end
+
+    event :reject do
+      transitions from: :under_moderation, to: :rejected
+    end
+
+    event :archive do
+      transitions from: %i[published under_moderation draft rejected], to: :archived
+    end
+  end
 
   belongs_to :category
   belongs_to :user
